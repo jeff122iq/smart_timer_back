9fd1aead88a297ace9750fc8831d8b15b012b357
develyour <develyour@gmail.com>
2021-02-09 09:04:36 +0200

    cors enabling

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NextFunction, Request, Response } from 'express';
 
 import { AppModule } from './app.module';
 import { Seeder } from './seeders/seeders';
@@ -9,13 +10,19 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
     app.enableCors();
+    app.use((req: Request, res: Response, next: NextFunction) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+    });
     const logger = app.get(Logger);
     const seeder = app.get(Seeder);
 
     seeder
       .seed()
       .then(() => logger.debug('Seeding complete!'))
-      .catch(err => {
+      .catch((err) => {
         logger.error('Seeding failed');
         throw err;
       });
